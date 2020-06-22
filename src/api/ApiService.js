a859196e1248ee136e44fc086b85490d98a50013
jamuar <jamuar@adobe.com>
2020-06-22 03:49:20 -0400

    service for youtube api

--- src/api/ApiService.js
+++ src/api/ApiService.js
@@ -6,6 +6,11 @@ class ApiService {
         console.log('uploading file')
         return axios.post("http://127.0.0.1:5001/file-upload", data);
     }
+
+    youtube(yotubeId) {
+        console.log('youtube song processing')
+        return axios.get("http://127.0.0.1:5001/youtube/"+yotubeId);
+    }
 }
 
 export default new ApiService();
